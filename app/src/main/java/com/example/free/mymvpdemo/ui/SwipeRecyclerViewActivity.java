4108e75c7513dafe2fc6e730130a04768696331c
dapeng <freewoandroid@163.com>
2017-08-24 00:17:52 +0800

    空白区域的点击事件

--- app/src/main/java/com/example/free/mymvpdemo/ui/SwipeRecyclerViewActivity.java
+++ app/src/main/java/com/example/free/mymvpdemo/ui/SwipeRecyclerViewActivity.java
@@ -2,13 +2,17 @@ package com.example.free.mymvpdemo.ui;
 
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.blankj.utilcode.util.ToastUtils;
 import com.example.free.mymvpdemo.R;
 import com.example.free.mymvpdemo.adapter.MyAdapter;
 import com.example.free.mymvpdemo.helper.Nav;
 import com.example.free.mymvpdemo.manager.BaseActivity;
+import com.example.free.mymvpdemo.view.recyclerview.RecyclerViewClickListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -47,6 +51,23 @@ public class SwipeRecyclerViewActivity extends BaseActivity {
                 Nav.toServiceActivity2(SwipeRecyclerViewActivity.this);
             }
         });
+
+        recyclerView.addOnItemTouchListener(new RecyclerViewClickListener(this,new RecyclerViewClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast.makeText(SwipeRecyclerViewActivity.this,"Click "+ listData.get(position),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemLongClick(View view, int position) {
+                Toast.makeText(SwipeRecyclerViewActivity.this,"Long Click "+listData.get(position),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onWhiteAreaClick() {
+                ToastUtils.showShort("点击了空白区域");
+            }
+        }));
     }
     protected void initData() {
         listData = new ArrayList<>();
